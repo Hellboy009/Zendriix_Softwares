87135bf7e78a6a828400787438223fa89f98898b
Hellboy009 <premendra.singh93@gmail.com>
2022-02-23 15:08:40 +0530

    Create dev.py

---
+++ dev.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+import os
+import patoolib
+
+os.chdir("/Users/prem/Documents/pyLearn")
+os.listdir()
+
+patoolib.extract_archive("File1.zip",outdir-"unpack")
+
+os.listdir(unpack)
